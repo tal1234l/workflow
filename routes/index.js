c9dcc5cd97eee50122f99aedff6885fb1bfd79ec
Tal Waserman <talwaserman@gmail.com>
2015-01-23 18:13:51 +0200

    add files

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,17 @@ var AllSchemas = require('../schemas/schemas');
 var validation = require('../validations/validations');
 var jwt        = require('jwt-simple');
 var mailin = require('mailin');
+var mandrill = require('mandrill-api/mandrill');
 
+/*
 mailin.start({
     port: 25,
     disableWebhook: true // Disable the webhook posting.
 });
 
-/* Event emitted after a message was received and parsed. */
+*/
+/* Event emitted after a message was received and parsed. *//*
+
 mailin.on('message', function (connection, data, content) {
     console.log(data);
     var record = new AllSchemas.Email(data);
@@ -22,6 +26,7 @@ mailin.on('message', function (connection, data, content) {
         }
     });
 });
+*/
 
 
 module.exports = function (flights) {
@@ -37,7 +42,7 @@ module.exports = function (flights) {
     };
     functions.email = function(req, res){
                 //number is not in DB
-                var record = new AllSchemas.Email(req);
+               /* var record = new AllSchemas.Email(req.body);
                 record.save(function(err,record){
                     if(err){
                         console.log(err);
@@ -45,7 +50,32 @@ module.exports = function (flights) {
                     } else {
                         res.status(200).json({req: req});
                     }
-                });
+                });*/
+
+        //-----------------------------//
+        // mandrill will send the replies in the request body as mandrill_events
+        var replies = JSON.parse(req.body.mandrill_events);
+
+        // here we are using async to process the replies, note that mandrill could
+        // send us a batch of replies, so we need to be able to process multiple replies
+        async.each(
+            replies,
+            function(reply, callback) {
+                // according to mandrill's docs, incoming emails will have the event of 'inbound'
+                if (reply.event === 'inbound') {
+                    processReply(reply, callback);  // we'll explain what to do here below
+                } else {
+                    callback();
+                }
+            },
+            function(err) {
+                // once we are done processing the replies, we should send mandrill
+                // the 200 status code, which means OK
+                res.send(200);
+            }
+        );
+
+        //----------------------------//
     };
     functions.createNewDIDNumber = function(req, res){
         var DID = req.param('DID');
@@ -167,3 +197,59 @@ function validateJWT(req,res){
 };
 
 
+function processReply(reply, processCallback) {
+    async.parallel({
+            user: function(callback) {
+                User.findOne({ email: reply.msg.from_email }, callback);
+            },
+            someObject: function(callback) {
+                // here we parse the id out of the email, which was in the reply-to field when we
+                // sent the original message
+                var objectId = parseObjectId(reply.msg.email);
+                SomeObject.findById(objectId, callback);
+            }
+        },
+        function(err, results) {
+            if (err) {
+                handleErrors(err, processCallback);
+            } else {
+
+                Message.create({
+                    content: removeQuotedText(reply.msg.text),  // we'll explain this function below
+                    createdById: results.user._id,
+                    timestamp: new Date(reply.ts*1000),  // mandrill returns a UTC unix timestamp,
+                    // we just need to multiply it by 1000 to
+                    // make it a regular date time
+                    objectId: results.someObject._id
+                }, processCallback);
+            }
+        });
+}
+function removeQuotedText(text) {
+    var delimeter = 'notification@sampleapp.com';
+
+    // escaping the dot in .com, so it doesn't affect our regex pattern below
+    delimeter.replace('.','\\.');
+
+    // this matches from the beginning of the email, multiple lines, until the line
+    // the has the delimeter, lines under the delimeter (including the delimeter line) won't match
+    var pattern = '.*(?=^.*' + delimeter + '.*$)';
+
+    // we are using XRegExp
+    var regex = xregexp(pattern, 'ims');
+
+    var delimeterFound = xregexp.test(text, regex);
+
+    if (delimeterFound) {
+        var match = xregexp.exec(text, regex);
+        return trimNewLines(match[0]);
+    } else {
+        return trimNewLines(text);
+    }
+}
+
+// email clients usually add extra white lines after the reply, this function removes empty
+// new lines before and after the passed in text
+function trimNewLines(text) {
+    return text.replace(/^\s+|\s+$/g, '');
+}
